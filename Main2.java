6f9c84e64c9e25be418359a88f4cf24d6fc52d50
roshanfaizal16 <148253222+roshanfaizal16@users.noreply.github.com>
2025-02-12 11:12:00 +0530

    Add files via upload
    created a new file

---
+++ Main2.java
@@ -0,0 +1,17 @@
+ public class Main{
+    public static void main(String[] args) {
+    byte a = 100;
+    byte b = 98;
+     byte add =(byte)(a+b);
+    byte sub = (byte)(b-a);
+     byte mul = (byte)(a*b);
+    byte div = (byte)(b/a);
+    
+    System.out.println("addition:"+add);
+    System.out.println("subtraction:"+sub);
+    System.out.println("multiplication:"+mul);
+    System.out.println("division:"+div);
+    
+
+    }
+}

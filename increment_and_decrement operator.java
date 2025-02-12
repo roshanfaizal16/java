cec6fbc429938f2c59fd0caa495bf837ce5ba785
roshanfaizal16 <148253222+roshanfaizal16@users.noreply.github.com>
2025-02-12 14:34:24 +0530

    Add files via upload
    created file using increment and decrement operator in java

---
+++ increment_and_decrement operator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class increment_and_decremnt_opertor{
+    public static void main(String[] args) {
+       int a = 10;
+       int b=  5;
+  
+       System.out.println(--a+a ++ + ++a +--a+ a++);
+            //System.out.println(a>>b);//
+    }
+}

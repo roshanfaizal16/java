55ff47ba14661a3f02d296621708360defef450c
roshanfaizal16 <148253222+roshanfaizal16@users.noreply.github.com>
2025-02-12 11:54:43 +0530

    Add files via upload
    short circuit and non short circuit logical operators for finding greatest of three numbers

---
+++ main3.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 34;
+        int c = 15;
+        System.out.print("Geratest of three  numbers is:");
+        if(a>b && a>c)
+        {
+            System.out.println(a);
+        }
+        else if(b>a && b>c)
+        {
+            System.out.println(b);
+        }
+        else 
+        System.out.println(c);
+    }
+}
+}

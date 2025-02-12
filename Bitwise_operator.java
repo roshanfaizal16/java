acb2f6f224485273ad890fd0430175c3625252e5
roshanfaizal16 <148253222+roshanfaizal16@users.noreply.github.com>
2025-02-12 12:44:00 +0530

    Add files via upload
    created file using bitwise operator

---
+++ Bitwise_operator.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Bitwise_operator {
+    public static void main(String[] args) {
+       int max;
+       int a= 492;
+       int b = 333;
+       int c=a&b;
+       int d = a^b;
+       int e = a|b;
+       
+       System.out.println(c);
+       System.out.println(d);
+       System.out.println(e);
+     
+    }
+}

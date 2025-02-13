ea6a4e117e447725c2422e328e0f11a67cb6c5a5
roshanfaizal16 <roshanfaizal16@gmail.com>
2025-02-13 15:43:03 +0530

    created a new file

--- Numberpattern.java
+++ Numberpattern.java
@@ -1,6 +1,6 @@
 class Numberpattern{
     public static void main(String[] args) {
-        int n = 5;
+        int n = 4;
          /*for (int i = 1; i<=n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("1");
@@ -20,19 +20,41 @@ class Numberpattern{
             }
              System.out.println(" ");
          }*/
-         for (int i = 1; i<=n; i++)
+        /*  for (int i = 1; i<=n; i++)
           {
             System.out.println(" ");
-            for (int j = n-i; j >= 1; j--)
+            for (int j =n-i; j >= 1; j--)
              {
                 System.out.print(" ");
                 
             }
             for(int k = 1;k<=i;k++)
             {
-                System.out.print("1");
+                System.out.print("*  ");
             }
              System.out.println();
-         }
+         }*/
+        int q = 0;
+         for (int i = 1; i<=n; i++)
+         {
+            System.out.println(" ");
+           for (int j =n-i; j >= 1; j--)
+            {
+               System.out.print(" ");
+               
+           }
+           for(int k = 1;k<=i+q;k++)
+           {
+            if(k==1||k==i+q||i ==n)
+               System.out.print("*");
+               else
+               System.out.print("0");
+           }
+           for(int l =n-i;l>=1;l--)
+           {
+            System.out.print(" ");
+           }
+           q++;
+        }
     } 
 }

d45494196306a5e55490a8e84221a6d71a690870
roshanfaizal16 <roshanfaizal16@gmail.com>
2025-02-13 16:05:14 +0530

    created a new file

--- Numberpattern1.java
+++ Numberpattern1.java
@@ -6,7 +6,10 @@ public class Numberpattern1 {
             System.out.print(" ");    
         }
         for (int k = 1; k<=(i*2-1); k++) {
-            System.out.print("*");   
+            if(k==1||k==(i*2-1))
+            System.out.print("*"); 
+            else
+            System.out.print(" ");  
         }
         System.out.println();
          }
@@ -18,7 +21,10 @@ public class Numberpattern1 {
             }
             for(int k = 1;k<=(2*i-1);k++)
             {
+                if(k==1||k==(2*i-1))
                 System.out.print("*");
+                else
+                System.out.print(" ");
             }
             System.out.println();
          }

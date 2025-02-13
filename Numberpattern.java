5e835540ea2ed9f8c254f822f418ca8987abfd9b
roshanfaizal16 <roshanfaizal16@gmail.com>
2025-02-13 12:06:57 +0530

    created a extra code

--- Numberpattern.java
+++ Numberpattern.java
@@ -1,7 +1,7 @@
 class Numberpattern{
     public static void main(String[] args) {
         int n = 5;
-         for (int i = 1; i<=n; i++) {
+         /*for (int i = 1; i<=n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("1");
                 
@@ -19,6 +19,20 @@ class Numberpattern{
                 
             }
              System.out.println(" ");
+         }*/
+         for (int i = 1; i<=n; i++)
+          {
+            System.out.println(" ");
+            for (int j = n-i; j >= 1; j--)
+             {
+                System.out.print(" ");
+                
+            }
+            for(int k = 1;k<=i;k++)
+            {
+                System.out.print("1");
+            }
+             System.out.println();
          }
     } 
 }

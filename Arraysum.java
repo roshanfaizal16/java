fe65dced647e06363250dedd45db1ee45e732c1e
roshanfaizal16 <roshanfaizal16@gmail.com>
2025-02-18 10:09:01 +0530

    created a new fle

---
+++ Arraysum.java
@@ -0,0 +1,28 @@
+public class Arraysum {
+     public static void main(String[] args) {
+         int[] a = {5,1,3,8,7};
+         int[] b = {9,7,7,7,9,4};
+         append(a, b);
+     }
+     static void append(int[] a,int[] b){
+        int[] c = new int[b.length];
+        int res =0;
+        int quo = 0;
+        for (int i = 0; i <a.length; i++) {
+          if(quo==0){
+            res  = a[i]+b[i];
+          }
+          else{
+            res = a[i]+b[i]+quo;
+          }  
+             c[i]= res%10;
+         quo = res/10;
+         }
+
+       
+        for(int k:c){
+            System.out.print(k+" ");
+           }
+        }
+    }
+

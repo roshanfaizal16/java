0fa17c65465b24a2bc25e0036396437ae2459464
roshanfaizal16 <roshanfaizal16@gmail.com>
2025-02-18 12:59:53 +0530

    created a new file

---
+++ Greatest_sum.java
@@ -0,0 +1,31 @@
+public class Greatest_sum {
+       public static void main(String[] args) {
+           int[] a ={1,1,1,0,1,1,0,1,1,0,1,1};
+           int[] b = {1,1,0 ,1};
+           int count = 0;
+           int value = 1;
+           boolean x = false;
+           while(value<=a.length-b.length){
+            x=true;
+           for (int i = count,j=0;j<b.length;i++,j++){
+           if(a[i]!=b[j]){
+                  x = false;
+                  break;
+           }
+           if(x){
+            break;
+           }
+           
+       }
+       count++;
+       value++;
+
+    }
+    if (x==true) {
+        System.out.println("yes");
+    }
+    else{
+        System.out.println("No");
+    }
+}
+}

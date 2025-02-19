a0f94e4a944da424b430fee4b8c7fc0e4232dbe6
roshanfaizal16 <roshanfaizal16@gmail.com>
2025-02-19 10:34:36 +0530

    created a new file

---
+++ .vscode/Double_array.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Double_array {
+    public static void main(String[] args) {
+        array();
+    }
+    static void array(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter rows and coloumns of array:");
+        int n = in.nextInt();
+        int m = in.nextInt();
+       int[][] arr = new int[n][m];
+        System.out.println("Enter the elements of the array");
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr[0].length;j++){
+               arr[i][j]=in.nextInt();
+            }
+        }
+        for(int i =0;i<arr.length;i++){
+            for(int j = 0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}

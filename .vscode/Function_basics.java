a3c2b4cd0762aa21ec65901496e6f303c09fdda3
roshanfaizal16 <roshanfaizal16@gmail.com>
2025-02-20 10:30:15 +0530

    created a new file

--- .vscode/Function_basics.java
+++ .vscode/Function_basics.java
@@ -3,18 +3,27 @@ import java.util.Scanner;
 
 public class Function_basics {
     Scanner in = new Scanner(System.in);
-    public void Details(){
-        
-        
+    public void Details(int m){
+        String[] name=new String[m];
+        int[] ida = new int[m]; 
         System.out.print("Enter Student name:");
-        String s = in.nextLine();
+        for(int i  = 0;i<m;i++){
+        name[i]= in.nextLine(); 
+    }
         System.out.print("Enter student ID:");
-         long n = in.nextInt();
-         System.out.println("Name: "+s);
-         System.out.println("ID: "+n);
+        for(int i  = 0;i<m;i++){
+            ida[i]= in.nextInt();
+        }
+         for(String  z:name){
+         System.out.print(z+" ");
+         }
+         System.out.println();
+         for(int b:ida){
+         System.out.print(b+" ");
+         }
     }
     public static void main(String[] args) {
         Function_basics std= new Function_basics();
-        std.Details();
+        std.Details(5);
     }
 }

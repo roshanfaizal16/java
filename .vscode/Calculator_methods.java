aeec1e4013299ea2e2f4c8f80b6e04b26f05c654
roshanfaizal16 <roshanfaizal16@gmail.com>
2025-02-21 11:01:10 +0530

    created a new file

---
+++ .vscode/Calculator_methods.java
@@ -0,0 +1,33 @@
+import  java.util.Scanner;
+public class Calculator_methods {
+    public int a,b,c;
+    Scanner in = new Scanner(System.in);
+    public void add(){
+        a = in.nextInt();
+        b = in.nextInt();
+       c = a+b;
+      System.out.println("Sum of two numbers:"+c);
+
+    }
+    public int difference(){
+        a = in.nextInt();
+        b = in.nextInt();
+        c = b-a;
+      return(c);
+    }
+    static  void product(int a,int b){
+        int c = a*b;
+        System.out.println("Product of two numbers"+c);
+    }
+    public int division(int a,int b){
+        float c=b/a;
+        return((int)c);
+    }
+    public static void main(String[] args) {
+        Calculator_methods obj = new Calculator_methods();
+        obj.add();
+        System.out.println("Difference of two numbers:"+obj.difference());
+        obj.product(5,4);
+        System.out.println("Division of two numbers:"+obj.division(6,97));
+    }
+}

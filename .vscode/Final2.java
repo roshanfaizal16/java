ba0b93d263dca6eb5dac97ec53b9cf1d5cee456a
roshanfaizal16 <roshanfaizal16@gmail.com>
2025-02-24 14:53:39 +0530

    created  a new file

--- .vscode/Final2.java
+++ .vscode/Final2.java
@@ -1,7 +1,21 @@
-abstract class  Final2 {
+
+abstract class  Final1 {
     final double pi ;
     static int r;
-    static void Final2(){
-        
+     Final1(){
+        this.pi = 3.14;
     }
-}
+    static void ins(){
+      r = 3254535;
+      System.out.println("Value of r:"+r);
+    }
+    }
+    class Final2 extends Final1{
+        public static void main(String[] args) {
+            
+       Final2 m = new Final2();
+       System.out.println(m.pi);
+       ins();
+        }
+    }
+

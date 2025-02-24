d1a4e60d781620814e0c2d091d171327147119ec
roshanfaizal16 <roshanfaizal16@gmail.com>
2025-02-24 14:38:17 +0530

    created a new file

---
+++ .vscode/Final2.java
@@ -0,0 +1,7 @@
+abstract class  Final2 {
+    final double pi ;
+    static int r;
+    static void Final2(){
+        
+    }
+}
--- .vscode/Savings_account.java
+++ .vscode/Savings_account.java
@@ -5,6 +5,9 @@ private long acc_number;
 private double balance;
 static double interest_rate;
 abstract void  calculate_Interest();
+void display(){
+    System.out.println("Account Number :"+acc_number);
+}
 }
  class Savings_account extends Bank_account {
     Scanner in = new Scanner(System.in);
@@ -20,12 +23,33 @@ abstract void  calculate_Interest();
         interest_rate = 2.55;
           double interest_amount = principle*n*interest_rate/100;
           double balance = principle+interest_amount;
-          System.out.println("Balance:"+balance);
+          System.out.println("Balance of savings account:"+balance);
     }
+}
+    class current_account extends Bank_account {
+        Scanner in = new Scanner(System.in);
+        void get(){
+            System.out.print("Enter account number:");
+            long acc_number = in.nextLong();
+        }
+        void calculate_Interest(){
+            System.out.print("Enter principle amount:");
+            double principle = in.nextDouble();
+            System.out.print("Number of months:");
+            int n = in.nextInt();
+            interest_rate = 3.50;
+              double interest_amount = principle*n*interest_rate/100;
+              double balance = principle+interest_amount;
+              System.out.println("Balance of current account:"+balance);
+        }
+    
     public static void main(String[] args) {
-        Savings_account m = new Savings_account();
+        current_account m = new current_account();
+        Savings_account n  = new Savings_account();
         m.get();
         m.calculate_Interest();
+        n.get();
+        n.calculate_Interest();
     }
     
 }
